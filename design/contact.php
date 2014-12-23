3b26d4e8ab0804d16007b021c1c039a921b1425f
Dorine B <Dorine@Oengus.local>
2014-12-23 11:25:17 +0100

    Essai pièce jointe 2

--- design/contact.php
+++ design/contact.php
@@ -379,12 +379,15 @@ function envoiemail($mail, $message_txt, $message_html){
     $header = "From: <$mail>".$passage_ligne;
     $header .= "Reply-to: <$mail>".$passage_ligne;
     $header .= "MIME-Version: 1.0".$passage_ligne;
-    $header .= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+    $header .= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
     
-    // Création du contenu
+    //=====Création du message.
     $message = $passage_ligne."--".$boundary.$passage_ligne;
-    $message .= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message .= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
+    $message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
+    //=====Ajout du message au format texte.
+    $message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
     $message.= $passage_ligne.$message_txt.$passage_ligne;
     
     $message.= $passage_ligne."--".$boundary.$passage_ligne;
